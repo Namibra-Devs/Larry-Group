8941967062ed21133c203f594312c0f697b90b1b
kindo <kindojnr58@gmail.com>
2024-09-18 14:09:52 +0000

    hero section done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,24 @@
 //  Navbar Section----------
  // Active menu----------
-// let menuLi = document.querySelectorAll('header ul li a');
-// let section = document.querySelectorAll('section');
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
 
 
-// function activeMenu(){
-//     let len = section.length;
-//     while(--len && window.scrollY + 97 < section[len].offsetTop){}
-//     menuLi.forEach(sec => sec.classList.remove("active"));
-//     menuLi[len].classList.add("active");
-//     }
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+    }
 
-//     activeMenu();
-//     window.addEventListener("scroll",activeMenu),
+    activeMenu();
+    window.addEventListener("scroll",activeMenu),
 
     // Sticky Navbar--------
     document.addEventListener("DOMContentLoaded", function() {
         const header = document.querySelector('header');
         window.addEventListener("scroll", function() {
-            header.classList.toggle("sticky", window.scrollY > 50);
+            header.classList.toggle("sticky", window.scrollY > 5);
         });
     });
  
@@ -41,4 +41,43 @@
      };
  });
 
-//  Hero Section Animation----------
+// JavaScript for the Hero Section
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("carousel-slide");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+// Auto-slide every 10 seconds
+setInterval(() => {
+    changeSlide(1);
+}, 8000);
+
+  
